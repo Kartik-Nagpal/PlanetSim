0e06f7acb1ed076b2566fd435fb26683de51f1f4
LightningLord4 <dumbguy44@gmail.com>
2016-08-10 22:41:38 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function modify_qty(val)
 	var qty = document.getElementById('qty').value;
     var new_qty = parseInt(qty, 10) + val;
     
-    if (new_qty < 0) 
+    if (new_qty < 0)
     {
     	new_qty = 0;
     }
